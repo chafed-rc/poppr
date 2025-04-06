e40fd42cdaf22054b8e0f788293371a88ad61201
chafed-rc <ryan.cuff@icloud.com>
2025-04-05 23:06:37 -0400

    remove mdx libs

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,15 +1,7 @@
-import createMDX from "@next/mdx";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-});
-
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
