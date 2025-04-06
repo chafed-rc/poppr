a488aa054a07d34d6979cb5c99bd226177500977
chafed-rc <ryan.cuff@icloud.com>
2025-04-06 00:03:15 -0400

    Updates to home page

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -7,7 +7,7 @@ import Usage from "@/components/usage";
 import Types from "@/components/types";
 export default function HomePage() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen ">
       <main className="flex-grow flex flex-col space-y-12 container mx-auto max-w-2xl px-4">
         <Hero />
         <Install />
